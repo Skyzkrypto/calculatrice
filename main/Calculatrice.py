3488717a0bcbc60831152be2eed8114e694e3c91
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-08-24 00:55:53 +0200

    oe

--- main/Calculatrice.py
+++ main/Calculatrice.py
@@ -1,10 +1,11 @@
 from tkinter import *
-
+import webbrowser
+import os
 # Boucle principale
 
-running = True
+running = bool(True)
 
-while running == True :
+if running == True :
 
     def calcul():
 
@@ -13,14 +14,17 @@ while running == True :
         def quitter():
  
             calculator.destroy()
-            running = False
-        
+        def git():
+            webbrowser.open_new(url="https://github.com/Wishrito/calculatrice")
+            
         # fenetre
-
         calculator = Tk()
-
+        
         # frame
-
+        bg = PhotoImage(file="src/img/1140270591828570112 (1).png")
+        label1 = Label(calculator, image = bg)
+        label1.place(x = 0, y = 0)
+        
         frame_calcul = Frame(calculator)
         frame_calcul.pack()
 
@@ -29,17 +33,21 @@ while running == True :
         calculator.minsize(250, 350)
         calculator.maxsize(650, 750)
         calculator.title("Calculatrice")
-        calculator.config(bg="#EACDC7")
-        # boutton   
-
+        calculator.iconbitmap(bitmap="src/img/1140270591828570112.ico")
+        # boutons
+        github = Button(calculator, text="voir sur Github", font=("arial", 25), command=git)
+        github.pack(side="top")
+        
         plus = Button(frame_calcul, text="+", font=("arial", 25))
+        plus.place()
         plus.pack(side="right")
 
         moins = Button(frame_calcul, text="-", font=("arial", 25))
         moins.pack(side="left")
 
         quiT = Button(calculator, text="quitter", font=("arial", 25), command=quitter)
-        quiT.pack(anchor="sw")
+        quiT.place(x=-10, y=-10)
+
 
         # mainloop fen
 
